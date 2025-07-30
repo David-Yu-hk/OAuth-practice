467f1adaba7a1c46f1524cd13951298a3b46e5e9
EnterYelz <scyu.david@gmail.com>
2025-07-31 05:16:12 +0800

    fix: ApplicationTests failed issue

--- src/main/java/com/oauthPractice/authorizetion/repository/UserRepository.java
+++ src/main/java/com/oauthPractice/authorizetion/repository/UserRepository.java
@@ -2,8 +2,11 @@ package com.oauthPractice.authorizetion.repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 import com.oauthPractice.authorizetion.model.User;
+import org.springframework.stereotype.Repository;
+
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends MongoRepository<User, String> {
     Optional<User> findByUsername(String username);
 }
